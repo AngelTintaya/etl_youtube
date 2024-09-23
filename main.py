d88e99486ab0af48c74e8d14bed4c153196305d8
AngelTintaya <a.tintaya.m@gmail.com>
2024-09-22 20:04:05 -0500

    ETL to store information into a DataBase

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import os
 import googleapiclient.discovery
 import googleapiclient.errors
-import configparser
 import pandas as pd
 
+from argparse import ArgumentParser
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+parser = ArgumentParser(description="Collect Youtube Comments")
+parser.add_argument('--videoid', type=str, default='mBoX_JCKZTE', help='The ID of the YouTube Video')
+args = parser.parse_args()
 
 load_dotenv()
 developer_key = os.getenv('DEVELOPER_KEY')
 api_service_name = os.getenv('API_SERVICE_NAME')
 api_version = os.getenv('API_VERSION')
+db_name = os.getenv('DB_NAME')
 
 def extract():
     print('==> EXTRACT')
@@ -19,7 +25,7 @@ def extract():
         part='snippet',
         maxResults=100,
         order="relevance",
-        videoId='mBoX_JCKZTE'
+        videoId=args.videoid
         )
     response = request.execute()
 
@@ -45,12 +51,23 @@ def transform(dict_data):
     return list_dict
 
 def load_server(list_data, file_path):
-    print('==> LOAD')
+    print('==> LOAD SERVER')
     df = pd.DataFrame(list_data)
     df.to_csv(file_path, index=False)
 
+def load_db(list_data, table_name):
+    print('==> LOAD SERVER')
+    database_location =f'sqlite:///database/{db_name}.sqlite'
+    engine = create_engine(database_location)
+
+    df = pd.DataFrame(list_data)
+    df['inserted_at'] = pd.Timestamp.now()
+
+    df.to_sql(table_name, engine, index=False, if_exists='append')
+
 if __name__ == '__main__':
-    file_path = os.path.join('.', 'server_outputs', 'youtube.csv')
+    file_name = 'youtube'
+    file_path = os.path.join('.', 'server_outputs', f'{file_name}.csv')
 
     # Extraccion
     dict_extracted = extract()
@@ -60,3 +77,4 @@ if __name__ == '__main__':
 
     # Carga
     load_server(list_transformed, file_path)
+    load_db(list_transformed, table_name=file_name)
